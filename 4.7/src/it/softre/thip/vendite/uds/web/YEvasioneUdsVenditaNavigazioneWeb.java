2361e507a3d783146ca3b142de7eef0df50b095c
Daniele aka COBRA <daniele.signoroni@softre.it>
2025-07-11 10:52:55 +0200

    mod

--- 4.7/src/it/softre/thip/vendite/uds/web/YEvasioneUdsVenditaNavigazioneWeb.java
+++
@@ -1,9 +0,0 @@
-package it.softre.thip.vendite.uds.web;
-
-import it.thera.thip.base.documenti.web.DocumentoNavigazioneWeb;
-
-public class YEvasioneUdsVenditaNavigazioneWeb extends DocumentoNavigazioneWeb {
-
-	public final static String cJspGestoreGriglia =
-		    "it/softre/thip/vendite/uds/GrigliaEstrazioneEvasione.jsp";
-}
